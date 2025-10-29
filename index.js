4e541d6e2c3e20932148c690b22a06fca7f3682b
Francisco <frangon152@gmail.com>
2025-10-29 13:27:06 -0300

    project

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/api/:date", function (req, res) {
   let date = new Date(req.params.date);
 
   if (isInvalidDate(date)) {
-    Date = new Date(+req.params.date);
+    date = new Date(+req.params.date);
   }
 
   if (isInvalidDate(date)) {
